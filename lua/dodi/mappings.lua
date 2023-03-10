dce94ba1fdcf7ffc6b9fb1b37cc28792fcade8cf
Dodilanne <dodilanne@ada-anvil.io>
2023-03-10 11:32:07 -0500

    Add useful mappings

--- lua/dodi/mappings.lua
+++ lua/dodi/mappings.lua
@@ -2,8 +2,16 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
+vim.keymap.set("n", "<A-j>", "Vyp")
+vim.keymap.set("n", "<A-k>", "VyP")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("i", "<C-h>", "<Left>")
+vim.keymap.set("i", "<C-j>", "<Down>")
+vim.keymap.set("i", "<C-k>", "<Up>")
+vim.keymap.set("i", "<C-l>", "<Right>")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
