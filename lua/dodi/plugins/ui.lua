d6731669997265910de0bca385e72aeb25e7cf69
Dodilanne <dodilanne@ada-anvil.io>
2023-03-14 11:25:34 -0400

    Add bufferline

--- lua/dodi/plugins/ui.lua
+++ lua/dodi/plugins/ui.lua
@@ -16,4 +16,20 @@ return {
     dependencies = { "kyazdani42/nvim-web-devicons" },
     opts = {},
   },
+
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+    },
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+      },
+    },
+  },
 }
