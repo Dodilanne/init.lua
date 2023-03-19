ca5502466a732a5645f9eb0b87fc596437274d24
Dodilanne <dodilanne@ada-anvil.io>
2023-03-19 10:44:59 -0400

    Rebuild lsp setup

---
+++ lua/dodi/plugins/lsp/init.lua
@@ -0,0 +1,175 @@
+return {
+	-- lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "folke/neoconf.nvim", cmd = "Neoconf", config = true },
+			{ "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
+			"mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		---@class PluginLspOpts
+		opts = {
+			-- options for vim.diagnostic.config()
+			diagnostics = {
+				underline = true,
+				update_in_insert = false,
+				virtual_text = { spacing = 4, prefix = "●" },
+				severity_sort = true,
+			},
+			-- Automatically format on save
+			autoformat = true,
+			-- options for vim.lsp.buf.format
+			-- `bufnr` and `filter` is handled by the DodiVim formatter,
+			-- but can be also overridden when specified
+			format = {
+				formatting_options = nil,
+				timeout_ms = nil,
+			},
+			-- LSP Server Settings
+			---@type lspconfig.options
+			servers = {
+				eslint = {
+					settings = {
+						-- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+						workingDirectory = { mode = "auto" },
+					},
+				},
+				jsonls = {},
+				lua_ls = {
+					-- mason = false, -- set to false if you don't want this server to be installed with mason
+					settings = {
+						Lua = {
+							workspace = {
+								checkThirdParty = false,
+							},
+							completion = {
+								callSnippet = "Replace",
+							},
+						},
+					},
+				},
+			},
+			-- you can do any additional lsp server setup here
+			-- return true if you don't want this server to be setup with lspconfig
+			---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+			setup = {
+				eslint = function()
+					require("dodi.util").on_attach(function(client)
+						if client.name == "eslint" then
+							client.server_capabilities.documentFormattingProvider = true
+						elseif client.name == "tsserver" then
+							client.server_capabilities.documentFormattingProvider = false
+						end
+					end)
+				end,
+			},
+		},
+		---@param opts PluginLspOpts
+		config = function(_, opts)
+			-- setup autoformat
+			require("dodi.plugins.lsp.format").autoformat = opts.autoformat
+			-- setup formatting and keymaps
+			require("dodi.util").on_attach(function(client, buffer)
+				require("dodi.plugins.lsp.format").on_attach(client, buffer)
+				require("dodi.plugins.lsp.mappings").on_attach(client, buffer)
+			end)
+
+			-- diagnostics
+			for name, icon in pairs(require("dodi.config").icons.diagnostics) do
+				name = "DiagnosticSign" .. name
+				vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+			end
+			vim.diagnostic.config(opts.diagnostics)
+
+			local servers = opts.servers
+			local capabilities =
+				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+			local function setup(server)
+				local server_opts = vim.tbl_deep_extend("force", {
+					capabilities = vim.deepcopy(capabilities),
+				}, servers[server] or {})
+
+				if opts.setup[server] then
+					if opts.setup[server](server, server_opts) then
+						return
+					end
+				elseif opts.setup["*"] then
+					if opts.setup["*"](server, server_opts) then
+						return
+					end
+				end
+				require("lspconfig")[server].setup(server_opts)
+			end
+
+			local have_mason, mlsp = pcall(require, "mason-lspconfig")
+			local available = have_mason and mlsp.get_available_servers() or {}
+
+			local ensure_installed = {} ---@type string[]
+			for server, server_opts in pairs(servers) do
+				if server_opts then
+					server_opts = server_opts == true and {} or server_opts
+					-- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
+					if server_opts.mason == false or not vim.tbl_contains(available, server) then
+						setup(server)
+					else
+						ensure_installed[#ensure_installed + 1] = server
+					end
+				end
+			end
+
+			if have_mason then
+				mlsp.setup({ ensure_installed = ensure_installed })
+				mlsp.setup_handlers({ setup })
+			end
+		end,
+	},
+
+	-- formatters
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = { "mason.nvim" },
+		opts = function()
+			local nls = require("null-ls")
+			return {
+				root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+				sources = {
+					nls.builtins.formatting.fish_indent,
+					nls.builtins.diagnostics.fish,
+					nls.builtins.formatting.stylua,
+					nls.builtins.formatting.shfmt,
+					nls.builtins.diagnostics.flake8,
+				},
+			}
+		end,
+	},
+
+	-- cmdline tools and lsp servers
+	{
+
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"shfmt",
+			},
+		},
+		---@param opts MasonSettings | {ensure_installed: string[]}
+		config = function(_, opts)
+			require("mason").setup(opts)
+			local mr = require("mason-registry")
+			for _, tool in ipairs(opts.ensure_installed) do
+				local p = mr.get_package(tool)
+				if not p:is_installed() then
+					p:install()
+				end
+			end
+		end,
+	},
+}
