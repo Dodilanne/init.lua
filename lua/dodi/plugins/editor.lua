7516972406564df3fb69742f6d461631f052744b
Dodilanne <dodilanne@ada-anvil.io>
2023-04-01 19:31:27 -0400

    Update telescope mappings

--- lua/dodi/plugins/editor.lua
+++ lua/dodi/plugins/editor.lua
@@ -78,19 +78,10 @@ return {
 				desc = "Find in Files (Grep)",
 			},
 			{ "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-			{
-				"<leader><space>",
-				Util.telescope("files"),
-				desc = "Find Files (root dir)",
-			},
 			-- find
 			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-			{
-				"<leader>ff",
-				Util.telescope("files"),
-				desc = "Find Files (root dir)",
-			},
-			{ "<leader>fF", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
+			{ "<leader><space>", Util.telescope("files", { cwd = false }), desc = "Find Files (root dir)" },
+			{ "<leader>ff", Util.telescope("files"), desc = "Find Files (cwd)" },
 			{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
 			-- git
 			{ "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
