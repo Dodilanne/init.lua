084a049dccad54f31f8723f3d615ae2b7cf76a3c
Dodilanne <dodilanne@ada-anvil.io>
2023-05-02 11:30:45 -0400

    Disable italics

--- lua/dodi/plugins/colorscheme.lua
+++ lua/dodi/plugins/colorscheme.lua
@@ -6,7 +6,9 @@ return {
 		lazy = false,
 		name = "rose-pine",
 		config = function()
-			require("rose-pine").setup()
+			require("rose-pine").setup({
+				disable_italics = true,
+			})
 			vim.cmd("colorscheme rose-pine")
 		end,
 	},
