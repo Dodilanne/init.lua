c9a45726dacb60c90046d71677285cfeea3e3f28
Dodilanne <dodilanne@ada-anvil.io>
2023-07-24 10:20:12 -0400

    Remap increment

--- lua/dodi/mappings.lua
+++ lua/dodi/mappings.lua
@@ -55,3 +55,6 @@ vim.keymap.set("n", "<leader>gu", ":Gitui<CR>", { noremap = true, silent = true,
 
 vim.keymap.set({ "n", "v", "i" }, "<C-s>", "<esc>:w<CR>", { desc = "Save file", silent = true })
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file", silent = true })
+
+-- Remap increment to prevent clashing with tmux prefix command
+vim.keymap.set({ "n", "v", "i" }, "<C-i>", "<C-a>", { desc = "Increment", silent = true })
