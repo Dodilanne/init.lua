f6ae9d605d4f4bcc9f08ae22fd5addd218f1fba8
Dodilanne <dodilanne@ada-anvil.io>
2023-10-08 11:53:28 -0400

    Tmux navigaor

--- lua/dodi/plugins/none-ls.lua
+++ lua/dodi/plugins/none-ls.lua
@@ -1,5 +1,6 @@
 return {
   "nvimtools/none-ls.nvim",
+  event = { "BufNewFile", "BufReadPost" },
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
@@ -7,6 +8,7 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.code_actions.eslint_d,
       },
     })
   end,
