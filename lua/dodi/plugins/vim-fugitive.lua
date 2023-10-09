7783cc389d91219079451b9851c5d2df66ede780
Dodilanne <dodilanne@ada-anvil.io>
2023-10-09 17:19:51 -0400

    Add keymaps

--- lua/dodi/plugins/vim-fugitive.lua
+++ lua/dodi/plugins/vim-fugitive.lua
@@ -3,5 +3,8 @@ return {
   cmd = { "G", "Git" },
   keys = {
     { "<leader>ge", ":Git ", desc = "Enter a git command" },
+    { "<leader>ga", ":Git add .<cr>", desc = "Stage all changes" },
+    { "<leader>gc", ':Git commit -m""<Left>', desc = "Quick commit" },
+    { "<leader>gp", ":Git push<cr>", desc = "Push" },
   },
 }
