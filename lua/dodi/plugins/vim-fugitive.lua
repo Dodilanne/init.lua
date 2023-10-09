3d9afa0fc415540e81e81d088377809eafd4c528
Dodilanne <dodilanne@ada-anvil.io>
2023-10-09 17:20:42 -0400

    Fix push command

--- lua/dodi/plugins/vim-fugitive.lua
+++ lua/dodi/plugins/vim-fugitive.lua
@@ -5,6 +5,6 @@ return {
     { "<leader>ge", ":Git ", desc = "Enter a git command" },
     { "<leader>ga", ":Git add .<cr>", desc = "Stage all changes" },
     { "<leader>gc", ':Git commit -m""<Left>', desc = "Quick commit" },
-    { "<leader>gp", ":Git push<cr>", desc = "Push" },
+    { "<leader>gP", ":Git push<cr>", desc = "Push" },
   },
 }
