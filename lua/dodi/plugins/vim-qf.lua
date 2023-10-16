0579fff1216eeea416e067e0885e82bc64311033
Vincent Audet <vincaudet@gmail.com>
2023-10-16 12:17:52 -0400

    Update mapping

--- lua/dodi/plugins/vim-qf.lua
+++ lua/dodi/plugins/vim-qf.lua
@@ -4,7 +4,7 @@ return {
     event = "VeryLazy",
     config = function()
       vim.keymap.set("n", "]q", "<Plug>(qf_qf_next)", { silent = true, desc = "Next quickfix item" })
-      vim.keymap.set("n", "[q", "<Plug>(qf_qf_prev)", { silent = true, desc = "Prev quickfix item" })
+      vim.keymap.set("n", "[q", "<Plug>(qf_qf_previous)", { silent = true, desc = "Previous quickfix item" })
       vim.keymap.set("n", "<leader>q", "<Plug>(qf_qf_toggle_stay)", { silent = true, desc = "Toggle quickfix window" })
     end,
   },
