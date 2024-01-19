9f3d1ddda2ff9362aefbe1d05a59ec0f724801e7
Dodilanne <dodilanne@ada-anvil.io>
2024-01-19 10:20:26 -0500

    Add fugitive keymap

--- lua/dodi/plugins/vim-fugitive.lua
+++ lua/dodi/plugins/vim-fugitive.lua
@@ -2,6 +2,7 @@ return {
   "tpope/vim-fugitive",
   cmd = { "G", "Git" },
   keys = {
+    { "<leader>gg", ":Git<cr>", desc = "Open fugitive window" },
     { "<leader>ge", ":Git ", desc = "Enter a git command" },
     { "<leader>ga", ":Git add .<cr>", desc = "Stage all changes" },
     { "<leader>gc", ':Git commit -m""<Left>', desc = "Quick commit" },
