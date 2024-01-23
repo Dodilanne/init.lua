c6f8f13a2921a6790adcc9634e0cf32d34604b9e
Dodilanne <dodilanne@ada-anvil.io>
2024-01-23 11:27:39 -0500

    Revert "Disable copilot"
    This reverts commit 28ecf61eb35e7f9fd8d4346b36d27dda8111dc3c.

--- lua/dodi/plugins/copilot.lua
+++ lua/dodi/plugins/copilot.lua
@@ -1,18 +1,18 @@
 return {
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   event = "InsertEnter",
-  --   cmd = "Copilot",
-  --   opts = {
-  --     suggestion = {
-  --       auto_trigger = true,
-  --       keymap = {
-  --         accept = "<m-j>",
-  --         accept_line = "<m-l>",
-  --         accept_word = "<m-k>",
-  --         dismiss = "<m-;>",
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    cmd = "Copilot",
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<m-j>",
+          accept_line = "<m-l>",
+          accept_word = "<m-k>",
+          dismiss = "<m-;>",
+        },
+      },
+    },
+  },
 }
