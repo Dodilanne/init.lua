9340bb210b97dfeb711010be893e587fd9d90c68
Dodilanne <dodilanne@ada-anvil.io>
2024-03-01 10:42:31 -0500

    Add copilot back

--- lua/dodi/plugins/copilot.lua
+++ lua/dodi/plugins/copilot.lua
@@ -1,18 +1,18 @@
 return {
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   event = "InsertEnter",
-  --   cmd = "Copilot",
-  --   opts = {
-  --     suggestion = {
-  --       auto_trigger = true,
-  --       keymap = {
-  --         accept = "<m-n>",
-  --         accept_line = "<m-e>",
-  --         accept_word = "<m-i>",
-  --         dismiss = "<m-o>",
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    cmd = "Copilot",
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<m-n>",
+          accept_line = "<m-e>",
+          accept_word = "<m-i>",
+          dismiss = "<m-o>",
+        },
+      },
+    },
+  },
 }
