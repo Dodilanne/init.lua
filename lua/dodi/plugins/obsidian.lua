981ed83e09f4bad5ec9bdf7a2e5aca02e9f14c6d
Dodilanne <dodilanne@ada-anvil.io>
2024-05-02 10:45:40 -0400

    Customize obsidian checkboxes

--- lua/dodi/plugins/obsidian.lua
+++ lua/dodi/plugins/obsidian.lua
@@ -25,6 +25,28 @@ return {
           path = "~/vaults/personal",
         },
       },
+      ui = {
+        checkboxes = {
+          [" "] = { order = 1001, char = "󰄱", hl_group = "ObsidianTodo" },
+          ["/"] = { order = 1002, char = "⧗", hl_group = "ObsidianProgress" },
+          ["x"] = { order = 1003, char = "", hl_group = "ObsidianDone" },
+          [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+          ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+        },
+        hl_groups = {
+          ObsidianTodo = { bold = true, fg = "#f78c6c" },
+          ObsidianDone = { bold = true, fg = "#89ddff" },
+          ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+          ObsidianTilde = { bold = true, fg = "#ff5370" },
+          ObsidianBullet = { bold = true, fg = "#89ddff" },
+          ObsidianRefText = { underline = true, fg = "#c792ea" },
+          ObsidianProgress = { bold = true, fg = "#c792ea" },
+          ObsidianExtLinkIcon = { fg = "#c792ea" },
+          ObsidianTag = { italic = true, fg = "#89ddff" },
+          ObsidianBlockID = { italic = true, fg = "#89ddff" },
+          ObsidianHighlightText = { bg = "#75662e" },
+        },
+      },
     },
   },
 }
