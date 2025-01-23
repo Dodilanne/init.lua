fae4213b1f2f1845fdba905d904a5e731e1c1f5d
Dodilanne <dodilanne@ada-anvil.io>
2025-01-23 12:58:14 -0500

    Try copilot

---
+++ lua/dodi/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "github/copilot.vim",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.cmd("Copilot disable")
+      vim.keymap.set("i", "<m-n>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
+      vim.keymap.set("i", "<m-i>", "<Plug>(copilot-dismisss)")
+      vim.keymap.set("i", "<m-h>", "<Plug>(copilot-accept-word)")
+      vim.keymap.set("i", "<m-l>", "<Plug>(copilot-accept-line)")
+      vim.keymap.set("i", "<m-e>", "<Plug>(copilot-suggest)")
+    end,
+  },
+}
